abb6c567b056995fa8252db592b4fdd4ac5a0922
Bart Baryla <bartekdoe@o2.pl>
2013-12-03 23:18:22 +0100

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,16 @@
-module ApplicationHelper
+module ApplicationHelper			# modules give us a way to package together 
+									#related methods, which can then be mixed in to Ruby classes using 
+									#include. When writing ordinary Ruby, you often write modules and
+									#include them explicitly yourself, but in the case of a helper module
+									#Rails handles the inclusion for us.
 
-  # Returns the full title on a per-page basis.
-  def full_title(page_title)
-    base_title = "Ruby on Rails Tutorial Sample App"
-    if page_title.empty?
-      base_title
+  # Returns the full title on a per-page basis.       # Documentation comment
+  def full_title(page_title)                          # Method definition
+    base_title = "Ruby on Rails Tutorial Sample App"  # Variable assignment
+    if page_title.empty?                              # Boolean test
+      base_title                                      # Implicit return
     else
-      "#{base_title} | #{page_title}"
+      "#{base_title} | #{page_title}"                 # String interpolation
     end
   end
 end
